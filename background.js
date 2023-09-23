caa4ead1c248fb061ff98b97c59061ed6e4c10c8
The Whale <thewhale@Thes-MacBook-Pro.local>
2023-09-23 00:31:40 -0700

    testing

--- background.js
+++ background.js
@@ -9,22 +9,38 @@ chrome.webRequest.onCompleted.addListener(
 )
 
 chrome.webRequest.onResponseStarted.addListener(details => {
-    console.log('%c Yo! we see a request', 'background: #faf')
-    console.log({details})
+    console.log('Yo! we see a request', 'background: #ff0')
+    console.log(JSON.stringify(details, null, 4))
 
     if (!details.responseHeaders.find(x => x.name.toLowerCase() === 'subscribe')) {
         return
     }
 
     var url = details.url
-    if (url[url.length-1] === '/') {
+    if (url[url.length - 1] === '/') {
         url = url.slice(0, -1)
     }
 
     var theTab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         theTab = tabs[0]
     })
 
-}, {urls:['<all_urls>']}, ["extraHeaders", 'responseHeaders'])
+}, { urls: ['<all_urls>'] }, ["extraHeaders", 'responseHeaders'])
 
+// chrome.webRequest.onBeforeSendHeaders.addListener(
+//     function (details) {
+//         console.log('onBeforeSendHeaders', 'background: #f0f')
+//         console.log(JSON.stringify(details, null, 4))
+
+//         // for (var i = 0; i < details.requestHeaders.length; ++i) {
+//         //     if (details.requestHeaders[i].name === 'User-Agent') {
+//         //         details.requestHeaders.splice(i, 1);
+//         //         break;
+//         //     }
+//         // }
+//         return { requestHeaders: details.requestHeaders };
+//     },
+//     { urls: ['<all_urls>'] },
+//     ['blocking', 'requestHeaders']
+// );

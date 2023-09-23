0911e8ec313ae52afb96b7cb0737d2e06cef3bc6
Michael Toomim <toomim@gmail.com>
2023-09-22 23:22:03 -0700

    Remove old code

--- background.js
+++ background.js
@@ -13,40 +13,5 @@ chrome.webRequest.onResponseStarted.addListener(details => {
         theTab = tabs[0]
     })
 
-    //
-    // Braid/sync9 setup
-    //
-    var identity = Braid.identity.random()
-
-    var s9 = Braid.sync9.create()
-
-    var queue = Braid.utils.createTxQueue(
-        (from, vid, parents, patches) => Braid.sync9.resolve_state(s9, from, vid, parents, patches),
-        async (tx, newState) => {
-            chrome.tabs.sendMessage(theTab.id, {
-                action: 'xyzzy',
-                data: newState,
-            }, function(err) {
-                if (err) {
-                    console.error('oh no ~>', err)
-                }
-            });
-        }
-    )
-
-    var braidClient = Braid.createPeer({
-        identity: identity,
-        httpHost: 'https://localhost:21232',
-        //webrtc: true,
-        onFoundPeersCallback: (peers) => {
-            console.log('found peers ~>', peers)
-        }
-    })
-
-    //braidClient.authorize().then(() => {
-        //console.log('braid authorized')
-        braidClient.subscribe('localhost:21231', '/', [ Braid.utils.genesisTxID ], queue.defaultTxHandler)
-    //})
-
 }, {urls:['<all_urls>']}, ["extraHeaders", 'responseHeaders'])
 

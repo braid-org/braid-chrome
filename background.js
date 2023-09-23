72d1fe17514a393687c800b9765c4bd6fb5d7a5c
Michael Toomim <toomim@gmail.com>
2023-09-22 23:49:18 -0700

    Sharing with greg

--- background.js
+++ background.js
@@ -1,4 +1,17 @@
+console.log('%c Yo! yo yo', 'background: #faf')
+
+chrome.webRequest.onCompleted.addListener(
+    details => {
+        console.log('%c Request completeorcycle', 'background: #ff8')
+    },
+    { urls: ["<all_urls>"], types: ["main_frame"] },
+    ["responseHeaders"]
+)
+
 chrome.webRequest.onResponseStarted.addListener(details => {
+    console.log('%c Yo! we see a request', 'background: #faf')
+    console.log({details})
+
     if (!details.responseHeaders.find(x => x.name.toLowerCase() === 'subscribe')) {
         return
     }

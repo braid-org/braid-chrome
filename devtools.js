40d812a8f53c60ea212d18af6eab43ed0bc60c19
Michael Toomim <toomim@gmail.com>
2023-10-20 22:01:55 -0700

    Tried (and failed) creating a sidebar from devtools elements

--- devtools.js
+++ devtools.js
@@ -4,5 +4,15 @@ chrome.devtools.panels.create(
     "128.png",
     "panel.html",
     function(panel) {
+        // console.log('panel created')
+        // chrome.devtools.panels.elements.createSidebarPane("BraidSide", function(sidebar) {
+        //     console.log('side bar created')
+        //     // Sidebar is now created
+        //     // You can set its content using the setPage or setObject methods
+        //     sidebar.setPage('sidebar.html'); // if you want to load an HTML page
+        //     // OR
+        //     // sidebar.setObject({foo: "bar"}, "My Object") // if you want to display an object
+        // })
     }
-);
+)
+
